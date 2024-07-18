0674825a8f2285c294d59572569db08e99835d8d
Mark Wang <armstrong.wang2000@gmail.com>
2024-07-17 20:24:08 -0600

    Make Archive all button more responsive

--- app/all-calls/page.tsx
+++ app/all-calls/page.tsx
@@ -3,7 +3,7 @@ import ActivityList from "@/components/activity_feed/ActivityList";
 export default function InboxPage() {
   return (
     <main className="flex flex-col items-center justify-between">
-      <ActivityList includingArchived={false} />
+      <ActivityList includingArchived={true} />
     </main>
   );
 }
--- components/activity_feed/ActivityList.tsx
+++ components/activity_feed/ActivityList.tsx
@@ -12,8 +12,12 @@ import { ActivityWithCounts } from "@/src/types/activity";
 import { Archive } from "lucide-react";
 import { useEffect, useState } from "react";
 
-const ActivityFeed = () => {
-  // const [activities, setActivities] = useState<ActivityWithCounts[]>([]);
+interface ActivityListProps {
+  includingArchived: boolean;
+}
+
+const ActivityList: React.FC<ActivityListProps> = ({ includingArchived }) => {
+  const isInAllCallsPage = includingArchived;
 
   const activities = useAppSelector(
     (state: RootState) => state.activities.activities
@@ -33,10 +37,10 @@ const ActivityFeed = () => {
   useEffect(() => {
     const groupedByDate = getLatestCallsWithCountsGroupByDate(
       activities,
-      false
+      includingArchived
     );
     setActivitiesGroupByDate(groupedByDate);
-  }, [activities]);
+  }, [activities, includingArchived]);
 
   if (status === "loading") {
     return <p>Loading activities...</p>;
@@ -46,9 +50,13 @@ const ActivityFeed = () => {
     return <p>Failed to load activities.</p>;
   }
 
-  function onArchiveAll(): void {
+  function onArchiveOrUnarchiveAll(): void {
+    // in the All Calls page, we want to unarchive all calls
+    // in the Inbox page, we want to archive all calls
+    const isArchived = !isInAllCallsPage;
+
     for (const activity of activities) {
-      dispatch(updateActivity({ activity, is_archived: true }));
+      dispatch(updateActivity({ activity, is_archived: isArchived }));
     }
   }
 
@@ -59,26 +67,35 @@ const ActivityFeed = () => {
         space-x-4"
       >
         <button
-          onClick={() => onArchiveAll()}
+          onClick={() => onArchiveOrUnarchiveAll()}
           className="flex items-center space-x-3 font-semibold
             text-gray-700 px-4 py-2  rounded-md hover:bg-blue-500
             hover:text-white focus:outline-none focus:border-blue-300"
         >
           <Archive size={24} className="text-gray-500" />
-          <div>Archive all calls</div>
+          <div>
+            {isInAllCallsPage ? "Unarchive all calls" : "Archive all calls"}
+          </div>
         </button>
       </header>
       <div className="space-y-4">
-        {activitiesGroupByDate.map((activitiesWithinDate) => (
-          <CallsWithinDate
-            key={activitiesWithinDate[0]}
-            callsWithinDate={activitiesWithinDate[1]}
-            date={activitiesWithinDate[0]}
-          />
-        ))}
+        {activitiesGroupByDate.length === 0 ? (
+          <p className="max-w-96 text-gray-500">
+            All calls are archived. See all calls including archived ones in the
+            All calls tab.
+          </p>
+        ) : (
+          activitiesGroupByDate.map((activitiesWithinDate) => (
+            <CallsWithinDate
+              key={activitiesWithinDate[0]}
+              callsWithinDate={activitiesWithinDate[1]}
+              date={activitiesWithinDate[0]}
+            />
+          ))
+        )}
       </div>
     </div>
   );
 };
 
-export default ActivityFeed;
+export default ActivityList;
